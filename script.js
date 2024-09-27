9dcf21f56961e1e09f2258bce8b326dabd0eba28
michael.shemesh <michael.shemesh@vcita.com>
2024-09-27 20:31:50 +0300

    Show seconds only if minutes and hours are 0

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ function fancyTimeFormat(duration) {
     // Hours, minutes and seconds
     const hrs = ~~(duration / 3600);
     const mins = ~~((duration % 3600) / 60);
+    const secs = ~~duration % 60;
 
     // Output like "1:01" or "4:03:59" or "123:03:59"
     let ret = "";
